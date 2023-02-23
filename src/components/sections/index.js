554b174870c4424e6450525cae5126c5b9abe60a
brian tarqui <tarquibrian@gmail.com>
2023-02-22 23:00:18 -0400

    initializating project

---
+++ src/components/sections/index.js
@@ -0,0 +1,5 @@
+export { default as Hero } from "./hero";
+export { default as About } from "./about";
+export { default as Contact } from "./contact";
+export { default as Experience } from "./experience";
+export { default as Projects } from "./projects";
