4a6de982db868f89f73c2bcf08be0ca9191f57a8
brian tarqui <tarquibrian@gmail.com>
2023-02-23 00:27:49 -0400

    seo, nav and footer added

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,16 @@
 import { GlobalStyle } from "@styles";
 import React from "react";
+import SEO from "./seo";
+import { Footer, Header } from "@components";
 
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyle />
+      <SEO title="Brian Tarqui Rojas" />
+      <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
