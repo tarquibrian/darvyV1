83a513b8b3f10a1db6edf47a8e5c978eca8d22f0
brian tarqui <tarquibrian@gmail.com>
2023-03-22 23:28:08 -0400

    hero buttons created

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -82,6 +82,44 @@ const HeroStyled = styled.section`
       font-size: clamp(14px, 2vw, 18px);
       font-weight: normal;
       line-height: 1.5;
+      margin-bottom: 1rem;
+    }
+
+    .hero__container-buttons {
+      display: flex;
+      gap: 1rem;
+
+      .button__resume,
+      .button__contact {
+        padding: 12px 18px;
+        display: grid;
+        place-content: center;
+        border: 1px solid rgba(255, 255, 255, 0.4);
+        border-radius: 4px;
+
+        &:hover {
+          background: rgba(255, 255, 255, 0.2);
+          cursor: pointer;
+        }
+      }
+
+      .button__resume {
+        border: 1px solid transparent;
+        background-image: linear-gradient(
+          93.3deg,
+          rgba(236, 80, 80, 1) 21.5%,
+          rgba(255, 97, 29, 1) 93.9%
+        );
+
+        &:hover {
+          border: 1px solid rgba(255, 255, 255, 0.4);
+          background-image: linear-gradient(
+            93.3deg,
+            rgba(206, 60, 60, 1) 21.5%,
+            rgba(235, 77, 9, 1) 93.9%
+          );
+        }
+      }
     }
 
     &:hover {
@@ -137,6 +175,20 @@ const Hero = () => {
           {Title}
           {SubTitle}
           {Description}
+          <div className="hero__container-buttons">
+            <a
+              className="button__resume"
+              href="./resumeV1.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Descargar CV
+            </a>
+
+            <a href="mailto:tarquibrian@gmail.com" className="button__contact">
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </HeroStyled>
