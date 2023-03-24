abc971caec2753d4802e3b07530126af1ae49497
brian tarqui <tarquibrian@gmail.com>
2023-03-23 21:34:07 -0400

    contact optimized

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -38,13 +38,37 @@ const ContactContainer = styled.div`
       background: #eae2b7;
     }
   }
+  &:hover::before {
+    opacity: 1;
+  }
+
+  &::before {
+    background: radial-gradient(
+      800px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.15),
+      transparent 40%
+    );
+    border-radius: inherit;
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0%;
+    left: 0%;
+    opacity: 0;
+    transition: opacity 500ms;
+    z-index: 0;
+  }
+
   &:hover {
-    background: rgba(255, 255, 255, 0.05)
-      linear-gradient(
-        to top right,
-        rgba(255, 255, 255, 0),
-        rgba(255, 235, 0, 0.15)
-      );
+    /* background: rgba(255, 255, 255, 0.05)
+        linear-gradient(
+          to top right,
+          rgba(255, 255, 255, 0),
+          rgba(255, 235, 0, 0.15)
+        ); */
+
+    background: rgba(255, 225, 142, 0.1);
   }
   @media screen and (max-width: 400px) {
     width: 90%;
