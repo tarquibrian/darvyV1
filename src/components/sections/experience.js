5320150f73894d4d8e06fc5c9f183aa44cca3124
brian tarqui <tarquibrian@gmail.com>
2023-03-23 21:32:51 -0400

    added mouse effect in experience section

--- src/components/sections/experience.js
+++ src/components/sections/experience.js
@@ -52,13 +52,37 @@ const ExperienceCard = styled.div`
     font-weight: 400;
   }
 
+  &:hover::before {
+    opacity: 1;
+  }
+
+  &::before {
+    background: radial-gradient(
+      800px circle at var(--mouse-x) var(--mouse-y),
+      rgba(255, 255, 255, 0.15),
+      transparent 40%
+    );
+    border-radius: inherit;
+    content: "";
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0%;
+    left: 0%;
+    opacity: 0;
+    transition: opacity 500ms;
+    z-index: 0;
+  }
+
   &:hover {
-    background: rgba(255, 255, 255, 0.05)
-      linear-gradient(
-        to top right,
-        rgba(255, 255, 255, 0),
-        rgba(255, 235, 0, 0.15)
-      );
+    /* background: rgba(255, 255, 255, 0.05)
+        linear-gradient(
+          to top right,
+          rgba(255, 255, 255, 0),
+          rgba(255, 235, 0, 0.15)
+        ); */
+
+    background: rgba(255, 225, 142, 0.1);
   }
   @media screen and (max-width: 768px) {
     min-width: fit-content;
