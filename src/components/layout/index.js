062025c1613eec79ca550f0ca4e9496a8ad65573
brian tarqui <tarquibrian@gmail.com>
2023-03-23 22:21:47 -0400

    component loader created

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,4 +3,5 @@ export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as LeftSide } from "./leftSide";
 export { default as RightSide } from "./rightSide";
+export { default as Loader } from "./loader";
 // export { default as SEO } from "./seo ";
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,12 @@
+import { Loader } from "@components";
+import { useEffect, useState } from "react";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(true), 6000);
+  }, []);
+
+  return <>{!loading ? <Component {...pageProps} /> : <Loader />}</>;
 }
