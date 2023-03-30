5f49fe155df6e069f01c570e4c0620d4810e7d33
brian tarqui <tarquibrian@gmail.com>
2023-03-29 23:03:30 -0400

    hero data

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -2,7 +2,7 @@ export const heroData = {
   es: {
     1: "Hola, mi nombre es",
     2: "Brian Tarqui Rojas",
-    3: "Desarrollador UX/UI - FullStack",
+    3: "Desarrollador Web FullStack",
     4: "Ingeniero de sistemas especializado en el desarrollo web FullStack, enfocado en crear y diseñar aplicaciones web modernas, priorizando siempre en entregar experiencias digitales excepcionales.",
     5: "Descargar CV",
     6: "Contacto",
@@ -10,7 +10,7 @@ export const heroData = {
   en: {
     1: "Hi, mi name is",
     2: "Brian Tarqui Rojas",
-    3: "UX/UI - FullStack Developer",
+    3: "FullStack Web Developer",
     4: "Systems Engineer specialized in FullStack web development, focused on creating and designing modern web applications, always prioritizing delivering exceptional digital experiences.",
     5: "Download CV",
     6: "Contact",
