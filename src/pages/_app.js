65fce5ce312395f2915e788178ffa96080f3f1bb
brian tarqui <tarquibrian@gmail.com>
2023-03-29 20:49:54 -0400

    tests

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,31 +2,35 @@ import { Loader } from "@components";
 import { motion, AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 import { AppProvider } from "src/context/app.context";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setLoading(true), 3000);
-  }, []);
+    if (router.asPath === "/") {
+      setTimeout(() => setLoading(true), 3000);
+    } else {
+      setLoading(true);
+    }
+  }, [router]);
 
   return (
     <AnimatePresence mode="sync">
-      {loading ? (
-        <AppProvider>
-          <Component {...pageProps} />
-        </AppProvider>
-      ) : (
+      {!loading ? (
         <motion.div
           key="loading"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ y: -1000 }}
-          // transition={{ duration: 1 }}
-          // style={{ background: "#404040" }}
         >
           <Loader />
         </motion.div>
+      ) : (
+        <AppProvider>
+          <Component {...pageProps} />
+        </AppProvider>
       )}
     </AnimatePresence>
   );
