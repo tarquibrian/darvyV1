50e799c1ebf0f28a50dd29d519388a85d5dd3ee0
brian tarqui <tarquibrian@gmail.com>
2023-04-12 21:40:58 -0400

    icon sass created

---
+++ src/components/icons/sass.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import imgIcon from "../../images/icon/sass.png";
+
+const IconSass = () => <Image src={imgIcon} alt="sass project icon" />;
+
+export default IconSass;
