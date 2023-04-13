f17ad8060446c4a41d389b00a4793246cdd6e096
brian tarqui <tarquibrian@gmail.com>
2023-04-12 21:45:02 -0400

    icon redux created

---
+++ src/components/icons/redux.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import imgIcon from "../../images/icon/redux.png";
+
+const IconRedux = () => <Image src={imgIcon} alt="redux project icon" />;
+
+export default IconRedux;
