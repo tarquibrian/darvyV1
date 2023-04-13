2804d8383e1e351a1f0acf0f75ddcfdd6060631a
brian tarqui <tarquibrian@gmail.com>
2023-04-13 14:56:37 -0400

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -80,6 +80,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head title="Darvy Portfolio">
+          <meta name="title" content="Darvy Portfolio"></meta>
           <meta
             name="description"
             content="Hi🤘, My name is Brian but they usually call me by my last name (Tarqui), I am a person who is very passionate about the world of technology and I recently obtained my degree in Systems Engineering."
