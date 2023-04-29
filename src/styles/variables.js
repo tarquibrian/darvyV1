2655eb9d21706da513ac4308643af5cbbd9c728a
brian tarqui <tarquibrian@gmail.com>
2023-04-28 22:32:59 -0400

    junk code removed

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,15 +2,6 @@ import { css } from "styled-components";
 
 const variables = css`
   :root {
-    /* --fz-xxs: 12px; */
-    /* --fz-xs: 13px; */
-    /* --fz-sm: 14px; */
-    /* --fz-md: 16px; */
-    /* --fz-lg: 18px; */
-    /* --fz-xl: 20px; */
-    /* --fz-xxl: 22px; */
-    /* --fz-heading: 32px; */
-
     /* FONTS */
     --ff-oswald: "Oswald", "Sofia Sans Condensed", "DM Sans", "Raleway",
       sans-serif;
