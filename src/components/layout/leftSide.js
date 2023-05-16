8ec6fce845b19e20c196cbbcb29bbedfe3c33c40
brian tarqui <tarquibrian@gmail.com>
2023-05-16 10:51:14 -0400

    test

--- src/components/layout/leftSide.js
+++ src/components/layout/leftSide.js
@@ -75,7 +75,7 @@ const SideStyle = styled(motion.div)`
         }
         &:hover {
           text-shadow: 0 0 5px rgba(255 255 255 / 0.5);
-          transform: scale(1.4);
+          transform: scale(1.15);
         }
       }
     }
