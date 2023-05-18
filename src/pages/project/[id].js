e07f83152b5311445fd4554af120b9c91df7e33d
brian tarqui <tarquibrian@gmail.com>
2023-05-17 21:20:16 -0400

    test

--- src/pages/project/[id].js
+++ src/pages/project/[id].js
@@ -1,6 +1,8 @@
 import { Layout } from "@components";
 import { useRouter } from "next/router";
 import React from "react";
+import { useAppContext } from "src/context/app.context";
+import { projectsData } from "src/data/projectsData";
 
 const languages = {
   es: projectsData.es,
@@ -8,13 +10,27 @@ const languages = {
 };
 
 const Project = () => {
+  const { state } = useAppContext();
   const router = useRouter();
 
   const currentLanguage = {
     lenguage: languages[state.currentLanguage],
   };
-  console.log(router.query);
-  return <Layout>Project</Layout>;
+
+  return (
+    <Layout>
+      <section>
+        {currentLanguage.lenguage.items
+          .filter((item) => item.id === router.query.id)
+          .map((item2, i) => (
+            <div key={i}>
+              title
+              <h1>{item2.title}</h1>
+            </div>
+          ))}
+      </section>
+    </Layout>
+  );
 };
 
 export default Project;
