09b72eeb75f750b597ce3cc0f9e92f0ac40ff5dc
brian tarqui <tarquibrian@gmail.com>
2023-05-17 21:01:59 -0400

    test

--- src/pages/project/[id].js
+++ src/pages/project/[id].js
@@ -2,8 +2,17 @@ import { Layout } from "@components";
 import { useRouter } from "next/router";
 import React from "react";
 
-const Project = ({ children }) => {
+const languages = {
+  es: projectsData.es,
+  en: projectsData.en,
+};
+
+const Project = () => {
   const router = useRouter();
+
+  const currentLanguage = {
+    lenguage: languages[state.currentLanguage],
+  };
   console.log(router.query);
   return <Layout>Project</Layout>;
 };
