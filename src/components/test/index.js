8123e74f1a9010c40c9b32b288175797c7f14f53
brian tarqui <tarquibrian@gmail.com>
2023-05-19 19:49:42 -0400

    test

--- src/components/test/index.js
+++ src/components/test/index.js
@@ -1,4 +1,3 @@
-"use client";
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
@@ -8,14 +7,6 @@ import { MathUtils } from "three";
 
 import vertexShader from "./vertexShader";
 import fragmentShader from "./fragmentShader";
-
-import { DotScreenShader } from "./customShader";
-
-// import { EffectComposer } from "@react-three/postprocessing";
-
-// import fragmentShader from "./fragmentShader";
-// import fragment from "./shader/fragment.glsl";
-// import vertex from "./shader/vertex.glsl";
 
 const fragment = `
 uniform float time;
@@ -126,53 +117,6 @@ void main() {
   vPosition = position;
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }`;
-
-const Blob = () => {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-  const hover = useRef(false);
-
-  const uniforms = useMemo(
-    () => ({
-      u_intensity: {
-        value: 0.3,
-      },
-      u_time: {
-        value: 0.0,
-      },
-    }),
-    []
-  );
-
-  useFrame((state) => {
-    const { clock } = state;
-    mesh.current.material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
-
-    mesh.current.material.uniforms.u_intensity.value = MathUtils.lerp(
-      mesh.current.material.uniforms.u_intensity.value,
-      hover.current ? 0.85 : 0.15,
-      0.02
-    );
-  });
-
-  return (
-    <mesh
-      ref={mesh}
-      position={[0, 0, 0]}
-      scale={1.5}
-      onPointerOver={() => (hover.current = true)}
-      onPointerOut={() => (hover.current = false)}
-    >
-      <icosahedronGeometry args={[2, 20]} />
-      <shaderMaterial
-        fragmentShader={fragmentShader}
-        vertexShader={vertexShader}
-        uniforms={uniforms}
-        wireframe={false}
-      />
-    </mesh>
-  );
-};
 
 const Effect = () => {
   const mesh = useRef();
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,10 @@ import {
   Projects,
   Contact,
 } from "@components";
-import Scene from "src/components/test";
-import styled from "styled-components";
 
-const Wrapper = styled.div`
-  /* background-color: lightblue; */
-  height: 900px;
-`;
 export default function Home() {
   return (
     <Layout>
-      <Wrapper className="test">
-        <Scene />
-      </Wrapper>
       <Hero />
       <About />
       <Experience />
