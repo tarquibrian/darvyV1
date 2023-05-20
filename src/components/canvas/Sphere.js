7756d20cadb7ded49b066c8e554af2477f87fdf3
brian tarqui <tarquibrian@gmail.com>
2023-05-19 22:12:12 -0400

    test

--- src/components/canvas/Sphere.js
+++ src/components/canvas/Sphere.js
@@ -5,11 +5,14 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { vertex } from "./shaders/vertex";
 import { fragment } from "./shaders/fragment";
 import { useAppContext } from "src/context/app.context";
+import { useControls } from "leva";
+import { LayerMaterial, Base, Depth, Fresnel } from "lamina";
 
 const Sphere = ({ color, setColor }) => {
   const { state } = useAppContext();
-  const newColor = state.color
+  const newColor = state.color;
   const mesh = useRef();
+  const materialRef = useRef();
   const uniforms = useMemo(
     () => ({
       // u_time: {
@@ -22,55 +25,26 @@ const Sphere = ({ color, setColor }) => {
     [state.color]
   );
 
-  // const updateColor = (valor) => {
-  //   let meshcolor = mesh.current.material.uniforms.color.value * 100;
-
-  //   if (meshcolor < valor) {
-  //     while (meshcolor < valor) {
-  //       meshcolor++;
-  //       mesh.current.material.uniforms.color.value = meshcolor / 100;
-  //     }
-  //   }
-  //   if (meshcolor > valor) {
-  //     while (meshcolor > valor) {
-  //       meshcolor--;
-  //       mesh.current.material.uniforms.color.value = meshcolor / 100;
-  //     }
-  //   }
-
-  //   console.log(meshcolor / 100);
-  // };
-
-  useEffect(() => {
-    mesh.current.material.uniforms.color.value = newColor;
-  }, [newColor]);
-  console.log('new', newColor);
-
   useFrame(() => {
     // const { clock } = state;
     // console.log(clock);
-    mesh.current.material.uniforms.time.value += 0.007;
-    // mesh.current.rotateZ(0.004);
-    mesh.current.rotation.z += 0.004;
-    mesh.current.material.uniforms.color.value = newColor;
+    // mesh.current.material.uniforms.time.value += 0.007;
+    // mesh.current.rotation.z += 0.004;
   });
-
-  // mesh.current.material.uniforms.color.value = valor;
-
-  // console.log(mesh.current.material.uniforms.color)
-
-  // setTimeout(() => {
-  //   update(0.0)
-  // }, 2000);
-
-  // setTimeout(() => {
-  //   update(0.4)
-  // }, 4000);
+  useEffect(() => {}, []);
+  
 
   return (
     <mesh ref={mesh}>
       <sphereBufferGeometry args={[1.5, 32, 32]} attach="geometry" />
-      <shaderMaterial
+      <LayerMaterial>
+        <Base color={base} alpha={1} mode="normal" />
+        <Depth colorA={colorB} colorB={colorA} alpha={0.5} mode="normal" near={0} far={3} origin={[1, 1, 1]} />
+        <Depth ref={depth} colorA={colorB} colorB="black" alpha={1} mode="lighten" near={0.25} far={2} origin={[1, 0, 0]} />
+        <Fresnel mode="softlight" color="white" intensity={0.3} power={2} bias={0} />
+      </LayerMaterial>
+      {/* <shaderMaterial
+        ref={materialRef}
         fragmentShader={fragment}
         vertexShader={vertex}
         side={THREE.DoubleSide}
@@ -78,7 +52,7 @@ const Sphere = ({ color, setColor }) => {
           derivatives: "#extension GL_OES_standard_derivatives : enable",
         }}
         uniforms={uniforms}
-      />
+      /> */}
     </mesh>
   );
 };
