f48af07d6674c1dad9dd9ff57ba7714ffbe34e81
brian tarqui <tarquibrian@gmail.com>
2023-05-24 17:21:42 -0400

    test

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,32 +1,10 @@
 import { Canvas } from "@react-three/fiber";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import Sphere from "./Sphere";
 import Effect from "./effect";
-import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useAppContext } from "src/context/app.context";
-
-const BGImage = styled(motion.div)`
-  position: fixed;
-  inset: 0;
-  width: 100vw;
-  height: 100vh;
-  z-index: -9;
-  overflow: hidden;
-  transform-style: preserve-3d;
-`;
 
 const ThreeCanvas = () => {
-  const ref = useRef();
-
   return (
-    // <BGImage
-    //   id="threejsBG"
-    //   ref={ref}
-    //   // initial={{ opacity: 0 }}
-    //   // animate={{ opacity: 1 }}
-    //   // transition={{ duration: 0.3 }}
-    // >
     <Canvas
       camera={{ position: [0, 0, 1.5] }}
       style={{
@@ -36,7 +14,6 @@ const ThreeCanvas = () => {
         height: "100vh",
         zIndex: "-9",
       }}
-      // onLoad={() => console.log('loaded')}
     >
       <Sphere />
       <Effect />
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,88 +1,35 @@
-import { GlobalStyle } from "@styles";
 import React, { useEffect, useRef, useState } from "react";
-import { Header, LeftSide, Loader, RightSide } from "@components";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useAppContext } from "src/context/app.context";
 import { useRouter } from "next/router";
 import Sketch from "src/threejs/main";
-import Image from "next/image";
-import noiseIMG from "../../images/layer-min.png";
-import dynamic from "next/dynamic";
-import ThreeCanvas from "../canvas";
 
 // const ThreeCanvas = dynamic(() => import("../canvas"), {
 //   loading: () => <p>loading..</p>,
 //   ssr: true,
 // });
 
-const BGImage = styled.div`
-  position: fixed;
-  inset: 0;
-  z-index: -9;
-  overflow: hidden;
-  transform-style: preserve-3d;
-
-  .bg {
-    display: block;
-    width: 100%;
-    height: 100%;
-    width: 100vw;
-    height: 100vh;
-    object-fit: cover;
-  }
-`;
-
-const WrapperMain = styled(motion.main)`
-  /* display: none; */
-  /* opacity: ${({ opacity }) => opacity}; */
-`;
+const WrapperMain = styled(motion.main)``;
 
 const Layout = ({ children }) => {
-  const { state, loadingComplete, updateColor } = useAppContext();
+  const { updateColor } = useAppContext();
   const router = useRouter();
-  const ref = useRef();
 
   useEffect(() => {
     if (router.asPath === "/") {
       updateColor([0.8, 0.95, 0.94]);
     }
-    console.log(router);
-  }, []);
+  }, [router.asPath]);
 
-  // useEffect(() => {
-  //   // console.log(state.loadingComplete);
-  //   // if (state.loadingComplete === false) {
-  //   //   setTimeout(() => {
-  //   //     // setLoading(true);
-  //   //     loadingComplete(true);
-  //   //   }, 4000);
-  //   // } else {
-  //   //   return;
-  //   // }
   //   // const Element = document.getElementById("threejsBG");
   //   // new Sketch({
   //   //   dom: Element,
   //   //   color: color,
   //   // });
-  //   // const updateBG = () => {
-  //   // setColor(0);
-  //   // BG.changeColor(color);
-  //   // };
-  //   // const renderBG = () => {
-  //   //   const Element = document.getElementById("threejsBG");
-  //   //   const BG = new Sketch({
-  //   //     dom: Element,
-  //   //     color: color,
-  //   //   });
-  //   // };
-  //   // renderBG();
-  // }, [state.loadingComplete, loadingComplete]);
-  // console.log(state.loadingComplete);
 
   return (
     <>
-      {/* {!state.loadingComplete && <Loader />} */}
       <WrapperMain
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
@@ -95,24 +42,6 @@ const Layout = ({ children }) => {
       >
         {children}
       </WrapperMain>
-      {/* <BGImage
-        id="threejsBG"
-        ref={ref}
-        // initial={{ opacity: 0 }}
-        // animate={{ opacity: 1 }}
-        // transition={{ duration: 0.3, ease: "easeIn" }}
-      >
-      </BGImage> */}
-      {/* <Image
-          priority
-          src={noiseIMG}
-          alt="background image"
-          placeholder="blur"
-          className="bg"
-          onLoadingComplete={() => {
-            setLoading(true);
-          }}
-        /> */}
     </>
   );
 };
