f08ee923704b581ccd30c9df2efd8905a64e18a0
brian tarqui <tarquibrian@gmail.com>
2023-06-07 00:12:24 -0400

    text

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -95,7 +95,7 @@ const NavbarContent = styled(motion.div)`
   height: 100%;
   width: 80%;
   max-width: 1280px;
-  padding: 0 4%;
+  padding: 0 3rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
